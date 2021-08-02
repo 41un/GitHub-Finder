bcdb4bdc8d2bc07915c33fd70cbf87c961269430
Alun Stern <alunsc@gmail.com>
2021-08-02 10:21:27 +0200

    added user components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import './App.css';
 
 class App extends Component {
-  foo = () => 'Bars';
+
 
   render() {
 
     return (
       <div className="App" >
         <Navbar />
+        <UserItem />
       </div>
     );
   }
